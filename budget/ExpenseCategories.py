73e146d623b8b52ff4f4c7ca0c88cd439e91551a
Mohamed Gharbaoui <m.gharbaoui@icloud.com>
2023-01-26 16:29:28 +0000

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -12,10 +12,10 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
-    timeit.timeit(stmt="expenses.categorize_for_loop()", setup='''from . import Expense
+    t1 = timeit.timeit(stmt="expenses.categorize_for_loop()", setup='''from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')''', number=100000, globals=globals())
-    timeit.timeit(stmt="expenses.categorize_set_comprehension()", setup='''from . import Expense
+    t2 = timeit.timeit(stmt="expenses.categorize_set_comprehension()", setup='''from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')''', number=100000, globals=globals())
     fig,ax=plt.subplots()
